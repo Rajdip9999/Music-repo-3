d6526fd6e3b0a0d91ad1bab00a8d8a8219cf38be
THEMADMAXPRO <157195522+THEMADMAXPRO@users.noreply.github.com>
2024-02-04 08:50:54 +0530

    Update ping.py

--- AarohiX/plugins/tools/ping.py
+++ AarohiX/plugins/tools/ping.py
@@ -10,6 +10,12 @@ from AarohiX.utils.decorators.language import language
 from config import BANNED_USERS, PING_IMG_URL
 
 
+# Define the repo and close buttons
+repo_button = InlineKeyboardButton("• ʀᴇᴘᴏ •", callback_data="gib_source")
+close_button = InlineKeyboardButton("• ᴄʟᴏsᴇ •", callback_data="close")
+
+
+@app.on_message(filters.command(["ping", "alive"]) & ~BANNED_USERS)
 @language
 async def ping_com(client, message: Message, _):
     start = datetime.now()
@@ -17,10 +23,26 @@ async def ping_com(client, message: Message, _):
         photo=PING_IMG_URL,
         caption=_["ping_1"].format(app.mention),
     )
-    pytgping = await Anony.ping()
+    pytgping = await Dil.ping()
     UP, CPU, RAM, DISK = await bot_sys_stats()
     resp = (datetime.now() - start).microseconds / 1000
     await response.edit_text(
         _["ping_2"].format(resp, app.mention, UP, RAM, CPU, DISK, pytgping),
-        reply_markup=supp_markup(_),
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [repo_button]
+            ]
+        ),
     )
+
+
+@app.on_callback_query(filters.regex("gib_source"))
+async def gib_repo_callback(_, callback_query):
+    await callback_query.edit_message_media(
+        media=InputMediaVideo("https://telegra.ph/file/b1367262cdfbcd0b2af07.mp4", has_spoiler=True),
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [close_button]
+            ]
+        ),
+                )
