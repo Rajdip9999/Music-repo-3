f57f2024f3e0fdac15d3648e63253da9ae0fc642
Rajdip9999 <101238383+Rajdip9999@users.noreply.github.com>
2024-02-11 21:07:23 +0530

    Update WishCutest.py

--- AarohiX/plugins/Management/WishCutest.py
+++ AarohiX/plugins/Management/WishCutest.py
@@ -4,7 +4,7 @@ import random
 import requests
 from AarohiX import app 
 
-SUPPORT_CHAT = "voiceofheart0"
+SUPPORT_CHAT = "lucifar_music"
 
 @app.on_message(filters.command("wish"))
 async def wish(_, m):
